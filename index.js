850b28bef24d536707237b049e4371f31f7b6a8a
jasenmichael <jasen@ready-builder.com>
2018-04-11 15:30:28 -0600

    added cors

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 const express = require('express')
 const fetch = require('node-fetch')
 const cheerio = require('cheerio')
+const cors = require('cors')
 
 const url = 'https://www.galvanize.com/denver-platte/events'
 
 const app = express()
+app.use(cors())
+
 
 app.get('/', (req, res) => {
   listEvents()

104f1a651fec8c7506a6688805b180ddcf1c74fd
Roberto Santos <robertosantos@Robertos-MacBook-Pro.local>
2018-04-12 12:56:07 -0600

    update map object

--- index.js
+++ index.js
@@ -65,58 +65,58 @@ function listEvents() {
 // const serverUrl = "http://localhost:3000"
 const serverUrl = "http://g-events-api.herokuapp.com"
 const maps = [{
-  LL: {
-    mapUrl: `${serverUrl}/map-images/LL-PlatteFloorMap.png`,
-    rooms: [{
-      id: 1,
-      name: "<room name here>",
-      roomMapUrl: "./images/LL-<room-id>-PlatteFloorMap.png"
-    }, {
-      id: 2,
-      name: "<room name here>",
-      roomMapUrl: "./images/LL-<room-id>-PlatteFloorMap.png"
-    }, {
-      id: 3,
-      name: "<room name here>",
-      roomMapUrl: "./images/LL-<room-id>-PlatteFloorMap.png"
-    }]
-  },
+          LL: {
+            mapUrl: `${serverUrl}/map-images/LL-PlatteFloorMap.png`,
+            rooms: [{
+              id: 1,
+              name: "Classroom 1",
+              roomMapUrl: `${serverUrl}/map-images/LL-PlatteFloorMap.png`
+            }, {
+              id: 2,
+              name: "Classroom 2",
+              roomMapUrl: `${serverUrl}/map-images/LL-PlatteFloorMap.png`
+            }, {
+              id: 3,
+              name: "Classroom 3",
+              roomMapUrl: `${serverUrl}/map-images/LL-PlatteFloorMap.png`
+            }]
+          },
 
-  L3: {
-    mapUrl: `${serverUrl}/map-images/L3-PlatteFloorMap.png`,
-    rooms: [{
-      id: 1,
-      name: "<room name here>",
-      roomMapUrl: "./images/L1-<room-id>-PlatteFloorMap.png"
-    }, {
-      id: 2,
-      name: "<room name here>",
-      roomMapUrl: "./images/L1-<room-id>-PlatteFloorMap.png"
-    }, {
-      id: 3,
-      name: "<room name here>",
-      roomMapUrl: "./images/L1-<room-id>-PlatteFloorMap.png"
-    }]
-  },
+          L3: {
+            mapUrl: `${serverUrl}/map-images/L3-PlatteFloorMap.png`,
+            rooms: [{
+              id: 1,
+              name: "some room 1 on the 3rd floor",
+              roomMapUrl: `${serverUrl}/map-images/L3-PlatteFloorMap.png`
+            }, {
+              id: 2,
+              name: "some room 2 on the 3rd floor",
+              roomMapUrl: `${serverUrl}/map-images/L3-PlatteFloorMap.png`
+            }, {
+              id: 3,
+              name: "some room 3 on the 3rd floor",
+              roomMapUrl: `${serverUrl}/map-images/L3-PlatteFloorMap.png`
+            }]
+          },
 
-  L4: {
-    mapUrl: `${serverUrl}/map-images/L4-PlatteFloorMap.png`,
-    rooms: [{
-      id: 1,
-      name: "<room name here>",
-      roomMapUrl: "./images/L1-<room-id>-PlatteFloorMap.png"
-    }, {
-      id: 2,
-      name: "<room name here>",
-      roomMapUrl: "./images/L1-<room-id>-PlatteFloorMap.png"
-    }, {
-      id: 3,
-      name: "<room name here>",
-      roomMapUrl: "./images/L1-<room-id>-PlatteFloorMap.png"
-    }]
-  }
+          L4: {
+            mapUrl: `${serverUrl}/map-images/L4-PlatteFloorMap.png`,
+            rooms: [{
+              id: 1,
+              name: "some room 1 on the 4th floor",
+              roomMapUrl: `${serverUrl}/map-images/L3-PlatteFloorMap.png`
+            }, {
+              id: 2,
+              name: "some room 2 on the 4th floor",
+              roomMapUrl: `${serverUrl}/map-images/L3-PlatteFloorMap.png`
+            }, {
+              id: 3,
+              name: "some room 3 on the 4th floor",
+              roomMapUrl: `${serverUrl}/map-images/L3-PlatteFloorMap.png`
+            }]
+          }
 
-}]
+        }]
 
 const port = process.env.PORT || 3000
 app.listen(port, () => {

f03867ed749f50313ff36c2598eb7c91e2d33a6f
jasenmichael <jasen@ready-builder.com>
2018-04-13 11:16:23 -0600

    created event route

---
+++ routes/events.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const router = express.Router();
+
+const queries = require('../event-queries');
+
+router.get("/", (request, response, next) => {
+    queries.list().then(events => {
+        response.json({events});
+    }).catch(next);
+});
+
+router.get("/:id", (request, response, next) => {
+    queries.read(request.params.id).then(event => {
+        event
+            ? response.json({event})
+            : response.status(404).json({message: 'Not found'})
+    }).catch(next);
+});
+
+router.post("/", (request, response, next) => {
+    queries.create(request.body).then(event => {
+        response.status(201).json({event});
+    }).catch(next);
+});
+
+router.delete("/:id", (request, response, next) => {
+    queries.delete(request.params.id).then(() => {
+        response.status(204).json({deleted: true});
+    }).catch(next);
+});
+
+router.put("/:id", (request, response, next) => {
+    queries.update(request.params.id, request.body).then(event => {
+        response.json({event});
+    }).catch(next);
+});
+
+module.exports = router;

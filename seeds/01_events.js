1a8dfea9dc01e3192a38d67eb0eab5e04c6ec15e
jasenmichael <jasen@ready-builder.com>
2018-04-16 19:00:17 -0600

    edit seeds

--- seeds/01_events.js
+++ seeds/01_events.js
@@ -2,8 +2,8 @@ exports.seed = function(knex, Promise) {
   return knex.raw('DELETE FROM "events"; ALTER SEQUENCE events_id_seq RESTART WITH 8;')
     .then(function() {
       return knex('events').insert([
-        { 
-          id: 1, 
+        {
+          id: 1,
           month: "December",
           day: 14,
           location: "Platte",
@@ -12,10 +12,10 @@ exports.seed = function(knex, Promise) {
           registerUrl: "https://www.meetup.com/Colorado-Open-Source-Operating-System-User-Group/",
           time: "Thursday, 6:00pm - 9:00pm MDT",
           category: "Data Science, Web Development",
-          description: "",
-          floor: "",
-          room: ""
-        }, 
+          description: "The official Denver Open Source Operating System User Group, held each month at Galvanize Denver Platte",
+          floor: "L3",
+          room: "Data Science Classroom"
+        },
         {
           id: 2,
           month: "Apr",
@@ -26,9 +26,9 @@ exports.seed = function(knex, Promise) {
           registerUrl: "https://www.eventbrite.com/e/galvanize-web-development-immersive-basic-training-denver-041418-tickets-44374500325",
           time: "Saturday, 9:00am - 3:00pm MDT",
           category: "Web Development",
-          description: "",
-          floor: "",
-          room: ""
+          description: "Do you want to learn to code but don’t know where to start? Have you done online tutorials that don’t make sense? Thinking about a career change into Web Development and want to see what coding is all about? Interested to experience what it’s like to be a Galvanize student for a day?! Join us for the FREE HTML/CSS/JavaScript Basic Training at Galvanize on Saturday, April 14th taught by professional instructors.",
+          floor: "LL",
+          room: "Classroom 2"
           },
           {
           id: 3,
@@ -40,9 +40,9 @@ exports.seed = function(knex, Promise) {
           registerUrl: "https://www.eventbrite.com/e/galvanize-denver-3-week-javascript-accelerated-course-41618-5318-tickets-43940753978",
           time: "",
           category: "Web Development",
-          description: "",
-          floor: "",
-          room: ""
+          description: "If you’re considering a career in Web Development or just looking to level up your current skills, this is the perfect place to start. Tried online tutorials with no success? Galvanize’s proven hands-on and community driven approach to Web Development education ensures thorough understanding of JavaScript’s core concepts every step of the way!",
+          floor: "LL",
+          room: "Classroom 2"
           },
           {
           id: 4,
@@ -54,9 +54,9 @@ exports.seed = function(knex, Promise) {
           registerUrl: "https://www.meetup.com/Galvanic-Blast-Magic-The-Gathering-at-Galvanize/",
           time: "Tuesday, 6:00pm - 8:00pm MDT",
           category: "Community",
-          description: "",
-          floor: "",
-          room: ""
+          description: "Come play some casual games of Magic: The Gathering at Galvanize Platte with a group of beginners and intermediate wizards. Bring your own decks and cards, or borrow some of ours. BYOB or you can get a drink at the bar (Gather). We plan to play Standard, Modern, EDH/Commander, and also do some Drafts or Sealed games. All very casual, so bring anything and anyone (Legacy, Vintage, etc. all welcome).",
+          floor: "L4",
+          room: "Atrium"
           },
           {
           id: 5,
@@ -68,9 +68,9 @@ exports.seed = function(knex, Promise) {
           registerUrl: "https://www.eventbrite.com/e/hack-the-dot-galvanize-platte-tickets-44494267552",
           time: "Tuesday, 6:00pm - 9:00pm MDT",
           category: "Web Development",
-          description: "",
-          floor: "",
-          room: ""
+          description: "A Hack the Dot is a two-hour hackathon where coding school students, junior developers, experienced developers, non developers, marketers, designers, and those with little or no computer programming skill come together to build ideas around a single domain name.",
+          floor: "LL",
+          room: "Classroom 2"
           },
           {
           id: 6,
@@ -82,9 +82,9 @@ exports.seed = function(knex, Promise) {
           registerUrl: "https://www.eventbrite.com/e/intro-to-htmlcssjavascript-free-learning-series-beginners-welcome-tickets-41218960019",
           time: "Wednesday, 6:00pm - 8:00pm MDT",
           category: "Web Development",
-          description: "",
-          floor: "",
-          room: ""
+          description: "It's back for the new year! The Learning Series is back and it's here to stay for 2018. We’ve built an incredible curriculum, created resources, and made learning interactive. This learning series format combine breakout sessions, formalized instruction, and group learning to enable to grasp the skills necessary to become a web developer.",
+          floor: "LL",
+          room: "Classroom 2"
           },
           {
           id: 7,
@@ -96,9 +96,9 @@ exports.seed = function(knex, Promise) {
           registerUrl: "https://www.meetup.com/Boulder-Denver-Big-Data/",
           time: "Thursday, 6:00pm - 8:30pm MDT",
           category: "Data Science",
-          description: "",
-          floor: "",
-          room: ""
+          description: "Come learn and/or share your experiences with NoSQL & Big Data technologies. No experience required.",
+          floor: "L3",
+          room: "Data Science Classroom"
         }
       ]);
     });
--- seeds/02_rooms.js
+++ seeds/02_rooms.js
@@ -4,58 +4,58 @@ exports.seed = function(knex, Promise) {
     .then(function () {
       return knex('rooms').insert([
         {
-            id: 1, 
+            id: 1,
             name: 'Classroom 1',
-            roomMapLandscapeUrl: 'http://g-events-api.herokuapp.com/map-images/LL-PlatteFloorLandscapeMap.png',
-            roomMapPortraitUrl: 'http://g-events-api.herokuapp.com/map-images/LL-PlatteFloorPortraitMap.png',
+            roomMapLandscapeUrl: './assets/maps/classroom-1-LandscapeMap.png',
+            roomMapPortraitUrl: './assets/maps/classroom-1-PortraitMap.png',
             level: 'LL'
         }, {
             id: 2,
             name: "Classroom 2",
-            roomMapLandscapeUrl: "http://g-events-api.herokuapp.com/map-images/LL-PlatteFloorLandscapeMap.png",
-            roomMapPortraitUrl: "http://g-events-api.herokuapp.com/map-images/LL-PlatteFloorPortraitMap.png",
+            roomMapLandscapeUrl: "./assets/maps/classroom-2-LandscapeMap.png",
+            roomMapPortraitUrl: "./assets/maps/classroom-2-PortraitMap.png",
             level: 'LL'
           },{
             id: 3,
             name: "Classroom 3",
-            roomMapLandscapeUrl: "http://g-events-api.herokuapp.com/map-images/LL-PlatteFloorLandscapeMap.png",
-            roomMapPortraitUrl: "http://g-events-api.herokuapp.com/map-images/LL-PlatteFloorPortraitMap.png",
+            roomMapLandscapeUrl: "./assets/maps/classroom-3-LandscapeMap.png",
+            roomMapPortraitUrl: "./assets/maps/classroom-3-PortraitMap.png",
             level: 'LL'
           },{
             id: 4,
             name: "Sonic",
-            roomMapLandscapeUrl: "http://g-events-api.herokuapp.com/map-images/L3-PlatteFloorLandscapeMap.png",
-            roomMapPortraitUrl: "http://g-events-api.herokuapp.com/map-images/L3-PlatteFloorPortraitMap.png",
+            roomMapLandscapeUrl: "./assets/maps/sonic-LandscapeMap.png",
+            roomMapPortraitUrl: "./assets/maps/sonic-PortraitMap.png",
             level: 'L3'
           },{
             id: 5,
             name: "1 Up",
-            roomMapLandscapeUrl: "http://g-events-api.herokuapp.com/map-images/L3-PlatteFloorLandscapeMap.png",
-            roomMapPortraitUrl: "http://g-events-api.herokuapp.com/map-images/L3-PlatteFloorPortraitMap.png",
+            roomMapLandscapeUrl: "./assets/maps/1-up-LandscapeMap.png",
+            roomMapPortraitUrl: "./assets/maps/1-up-PortraitMap.png",
             level: 'L3'
           },{
             id: 6,
             name: "Data Science Classroom",
-            roomMapLandscapeUrl: "http://g-events-api.herokuapp.com/map-images/L3-PlatteFloorLandscapeMap.png",
-            roomMapPortraitUrl: "http://g-events-api.herokuapp.com/map-images/L3-PlatteFloorPortraitMap.png",
+            roomMapLandscapeUrl: "./assets/maps/data-science-classroom-LandscapeMap.png",
+            roomMapPortraitUrl: "./assets/maps/data-science-classroom-PortraitMap.png",
             level: 'L3'
           },{
             id: 7,
             name: "Atrium",
-            roomMapLandscapeUrl: "http://g-events-api.herokuapp.com/map-images/L4-PlatteFloorLandscapeMap.png",
-            roomMapPortraitUrl: "http://g-events-api.herokuapp.com/map-images/L4-PlatteFloorPortraitMap.png",
+            roomMapLandscapeUrl: "./assets/maps/atrium-LandscapeMap.png",
+            roomMapPortraitUrl: "./assets/maps/atrium-PortraitMap.png",
             level: 'L4'
           },{
             id: 8,
             name: "Mega Man",
-            roomMapLandscapeUrl: "http://g-events-api.herokuapp.com/map-images/L4-PlatteFloorLandscapeMap.png",
-            roomMapPortraitUrl: "http://g-events-api.herokuapp.com/map-images/L4-PlatteFloorPortraitMap.png",
+            roomMapLandscapeUrl: "./assets/maps/mega-man-LandscapeMap.png",
+            roomMapPortraitUrl: "./assets/maps/mega-man-PortraitMap.png",
             level: 'L4'
           },{
             id: 9,
             name: "Gold Coin",
-            roomMapLandscapeUrl: "http://g-events-api.herokuapp.com/map-images/L4-PlatteFloorMap.png",
-            roomMapPortraitUrl: "http://g-events-api.herokuapp.com/map-images/L4-PlatteFloorPortraitMap.png",
+            roomMapLandscapeUrl: "./assets/maps/gold-coin-LandscapeMap.png",
+            roomMapPortraitUrl: "./assets/maps/gold-coin-PortraitMap.png",
             level: 'L4'
           }
       ]);
